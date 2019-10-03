0bab1aa5454a150a0ad51058a2271b1cb6852f19
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2019-10-03 15:54:03 -0300

    Removed old video labels

--- config/label/video.js
+++ config/label/video.js
@@ -1,10 +1,6 @@
 module.exports = {
-  open: '[aria-label="Share webcam"]',
-  menu: {
-    share: 'img[src="/html5client/resources/images/video-menu/icon-webcam-off.svg"]',
-    unshare: 'img[src="/html5client/resources/images/video-menu/icon-webcam-off.svg"]',
-    swap: 'img[src="/html5client/resources/images/video-menu/icon-swap.svg"]'
-  },
+  share: '[aria-label="Share webcam"]',
+  unshare: '[aria-label="Share webcam"]',
   settings: {
     start: '[aria-label="Start sharing"]'
   }
