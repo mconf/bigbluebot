cf269d464ebe54dce802cb4baca890e5db4991c0
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2019-10-20 14:06:37 -0300

    Better logging

--- config/label/audio.js
+++ config/label/audio.js
@@ -2,7 +2,7 @@ module.exports = {
   join: '[aria-label="Join Audio"]',
   dialog: {
     microphone: '[aria-label="Microphone"]',
-    listen: '[aria-label="Listen Only"]',
+    listen: '[aria-label="Listen only"]',
     close: '[aria-label="Close Join audio modal"]',
     modal: '[aria-label="Join audio modal"]'
   },
