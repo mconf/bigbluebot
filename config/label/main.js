9c336a0c3ba8dd8a8d99e927305f7605646ba2b4
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2019-11-20 11:38:33 -0300

    Changed the first selector to wait and browser's autoplay policy

--- config/label/main.js
+++ config/label/main.js
@@ -9,5 +9,8 @@ module.exports = {
     poll: 'i.itemIcon--Z207zn1.icon-bbb-user',
     upload: 'i.itemIcon--Z207zn1.icon-bbb-presentation',
     breakout: 'i.itemIcon--Z207zn1.icon-bbb-rooms'
+  },
+  options: {
+    button: '[aria-label="Options"]'
   }
 }
