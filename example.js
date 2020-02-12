f9ffff67ccaf1a42e35ea6a09d5e7bd627734ff0
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2020-02-12 12:24:19 -0300

    Avoid trying to close the browser if it's connected by an websocket endpoint

--- example.js
+++ example.js
@@ -7,7 +7,9 @@
 const bigbluebot = require('./index.js')
 
 let actions = async page => {
+  await bigbluebot.audio.modal.microphone(page)
   await bigbluebot.video.join(page)
+  await bigbluebot.chat.send(page)
 }
 
 bigbluebot.run(actions)
